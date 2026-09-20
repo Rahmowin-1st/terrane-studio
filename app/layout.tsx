6d4dda306e3f159a40a85dccb23718b87e88dd7e
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-09-20 14:20:43 +0500

    design: switch runtime to Terrane final visual authority

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./v5.css";
+import "./final.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { LightExperience } from "@/components/LightExperience";
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="antialiased terrane-v5">
+    <html lang="en" className="antialiased terrane-final">
       <head>
         <link rel="preconnect" href="https://images.unsplash.com" />
       </head>
