7f5e82c032457795ed97179618aec8092cc1ef0c
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-09-20 17:05:55 +0500

    rebuild: make HomeExperience the canonical homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import { Hero } from "@/components/sections/Hero";
-import { Trust } from "@/components/sections/Trust";
-import { Work } from "@/components/sections/Work";
-import { Disciplines } from "@/components/sections/Disciplines";
-import { Control } from "@/components/sections/Control";
-import { Approach } from "@/components/sections/Approach";
-import { Studio } from "@/components/sections/Studio";
-import { Commission } from "@/components/sections/Commission";
+import { HomeExperience } from "@/components/HomeExperience";
 
 export default function Home() {
-  return (
-    <main id="main">
-      <Hero />
-      <Trust />
-      <Work />
-      <Disciplines />
-      <Control />
-      <Approach />
-      <Studio />
-      <Commission />
-    </main>
-  );
+  return <HomeExperience />;
 }
